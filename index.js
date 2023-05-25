20d399efa3baced1c65678d6e67461e7d432e31b
anju <anjualfino@gmail.com>
2023-05-26 00:35:13 +0800

    functionalities added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Packages for this application
 let inquirer = require('inquirer');
-const fs = require('fs'); 
+const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes.js');
 
 //Questions for user input
@@ -17,63 +17,63 @@ const questions = [
         }
     },
     {
-         type: 'input',
-         message: 'Please enter the text color (color can be keyword OR a hexadecimal number)?',
-         name: 'textcolor',
-         validate: function (value) {
-             // Regular expression for color keywords
-             const colorKeywordRegex = /^(red|blue|green|yellow|orange|purple|pink|black|white|gray)$/i;
- 
-             // Regular expression for hexadecimal color codes
-             const hexCodeRegex = /^#([0-9A-F]{3}){1,2}$/i;
- 
-             if (value.match(colorKeywordRegex) || value.match(hexCodeRegex)) {
-                 return true; // Valid color input
-             } else {
-                 return 'Please enter a valid color'; // Invalid color input
-             }
-         }
-     },
-     {
-         type: 'list',
-         message: 'Please select a shape:',
-         name: 'shape',
-         choices: ['circle', 'triangle', 'square'],
- 
-         validate: function (value) {
-             if (value.length == 0) {
-                 return 'Please enter a valid input';
-             }
-             return true;
-         }
-     },
-     {
-         type: 'input',
-         message: 'Please enter shape color (color can be keyword OR a hexadecimal number)?',
-         name: 'shapecolor',
-         validate: function (value) {
-             // Regular expression for color keywords
-             const colorKeywordRegex = /^(red|blue|green|yellow|orange|purple|pink|black|white|gray)$/i;
- 
-             // Regular expression for hexadecimal color codes
-             const hexCodeRegex = /^#([0-9A-F]{3}){1,2}$/i;
- 
-             if (value.match(colorKeywordRegex) || value.match(hexCodeRegex)) {
-                 return true; // Valid color input
-             } else {
-                 return 'Please enter a valid color'; // Invalid color input
-             }
-         }
-     }, 
+        type: 'input',
+        message: 'Please enter the text color (color can be keyword OR a hexadecimal number)?',
+        name: 'textcolor',
+        validate: function (value) {
+            // Regular expression for color keywords
+            const colorKeywordRegex = /^(red|blue|green|yellow|orange|purple|pink|black|white|gray)$/i;
+
+            // Regular expression for hexadecimal color codes
+            const hexCodeRegex = /^#([0-9A-F]{3}){1,2}$/i;
+
+            if (value.match(colorKeywordRegex) || value.match(hexCodeRegex)) {
+                return true;
+            } else {
+                return 'Please enter a valid color';
+            }
+        }
+    },
+    {
+        type: 'list',
+        message: 'Please select a shape:',
+        name: 'shape',
+        choices: ['circle', 'triangle', 'square'],
+
+        validate: function (value) {
+            if (value.length == 0) {
+                return 'Please enter a valid input';
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please enter shape color (color can be keyword OR a hexadecimal number)?',
+        name: 'shapecolor',
+        validate: function (value) {
+            // Regular expression for color keywords
+            const colorKeywordRegex = /^(red|blue|green|yellow|orange|purple|pink|black|white|gray)$/i;
+
+            // Regular expression for hexadecimal color codes
+            const hexCodeRegex = /^#([0-9A-F]{3}){1,2}$/i;
+
+            if (value.match(colorKeywordRegex) || value.match(hexCodeRegex)) {
+                return true; // Valid color input
+            } else {
+                return 'Please enter a valid color'; // Invalid color input
+            }
+        }
+    },
 
 ];
-//function to create svg file
+/* //function to create svg file
 function writeToFile(data) {
     fs.writeFile('logo.svg', data,
         (err) => err ? console.error(err) : console.log('Generated logo.svg!!')
     );
 }
-
+ */
 
 //function to initialize app
 function init() {
@@ -84,30 +84,39 @@ function init() {
     // Prompt the questions to the user
     inquirer.prompt(questions)
         .then((answers) => {
-            console.log(answers);
-            switch(answers.shape){
+            let shape;
+            switch (answers.shape) {
                 case 'circle':
-                    const circle = new Circle(answers);
-                    circle.saveSvgHeader();
-                    circle.addSvgCircle();
-                    circle.saveSvgText();
+                    shape = new Circle();
                     break;
                 case 'triangle':
-                    const triangle = new Triangle(answers);
-                    triangle.saveSvgHeader();
-                    triangle.addSvgTriangle();
-                    triangle.saveSvgText();
+                    shape = new Triangle(answers);
                     break;
                 case 'square':
-                    const square = new Square(answers);
-                    square.saveSvgHeader();
-                    square.addSvgSquare();
-                    square.saveSvgText();
+                    shape = new Square(answers);
                     break;
                 default:
                     break;
             }
-            console.log("logo.svg successfully generated..")
+            /*  shape.setShape(answers.shape,answers.shapecolor);
+             shape.setText(answers.text,answers.textcolor);
+             shape.saveSvgHeader();
+             shape.addSvgData();
+             shape.saveSvgText();
+             console.log(" Generated logo.svg successfully..") */
+
+            shape.setShape(answers.shape, answers.shapecolor)
+                .then(() => shape.setText(answers.text, answers.textcolor))
+                .then(() => shape.saveSvgHeader())
+                .then(() => shape.addSvgData())
+                .then(() => shape.saveSvgText())
+                .then(() => {
+                    console.log(" Generated logo.svg successfully..");
+                })
+                .catch((error) => {
+                    console.error("An error occurred:", error);
+                });
+
 
         })
         .catch((error) => {
